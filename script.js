915029abcddae7711114e4f751c41da6c0955a2a
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-02-12 14:29:36 +0200

    132nd Commit

--- script.js
+++ script.js
@@ -59,6 +59,11 @@ function createTable() {
         if (snakeLine.x >= 1 && snakeLine.x <= 15 && snakeCol.y >= 1 && snakeCol.y <= 17) {
             if (event.key == 'ArrowRight' && arrowLeft == false) {
                 arrowRight = true;
+                clearInterval(interval); 
+                interval = setInterval(function output() {
+                    moveSnake(interval, snake, len, snakeLine, snakeCol, fruitLine, fruitCol,
+                        eatenFruit, isSnakeBody, arrowRight, arrowUp, arrowLeft, arrowDown);
+                }, 300);
             } else if (event.key == 'ArrowLeft' && arrowRight == false) {
                 arrowLeft = true;
             }
@@ -67,14 +72,14 @@ function createTable() {
             } else if (event.key == 'ArrowDown' && arrowUp == false) {
                 arrowDown = true;
             }
-            if ((event.key == 'ArrowRight' && arrowLeft == false) || (event.key == 'ArrowLeft' && arrowRight == false) 
+            /*if ((event.key == 'ArrowRight' && arrowLeft == false) || (event.key == 'ArrowLeft' && arrowRight == false) 
             || (event.key == 'ArrowUp' && arrowDown == false) || (event.key == 'ArrowDown' && arrowUp == false)) {
                 clearInterval(interval); 
                 interval = setInterval(function output() {
                     moveSnake(interval, snake, len, snakeLine, snakeCol, fruitLine, fruitCol,
                         eatenFruit, isSnakeBody, arrowRight, arrowUp, arrowLeft, arrowDown);
                 }, 300);
-            }
+            }*/
         }
         /*if (event.key == 'ArrowRight' && arrowLeft == false && snakeLine.x >= 1 && snakeLine.x <= 15 &&
             snakeCol.y >= 1 && snakeCol.y <= 17) {
