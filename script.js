85730a1c61f4b27430fe9c9e30fc3927de6d7d24
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-02-15 10:34:22 +0200

    225th Commit

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ function randomFruit(snake, len, snakeLine, snakeCol, fruitLine, fruitCol, eaten
         let neighbour = null;
         const incrementLine = {value: 0}, incrementCol = {value: 0};
         modifyLineAndColumn(direction, incrementLine, incrementCol);
-        neighbour = document.getElementById((snakeLine.x + incrementLine) * 100 + (snakeCol.y + incrementCol));
+        neighbour = document.getElementById((snakeLine.x + incrementLine.value) * 100 + (snakeCol.y + incrementCol.value));
         if (neighbour != null && neighbour.style.backgroundColor == 'yellow') {
             isSnakeBody.value = true;
         }
