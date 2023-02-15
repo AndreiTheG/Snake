19a5b4450c313449cca164aee30faad2b4cdee3e
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-02-15 10:03:58 +0200

    220th Commit

--- script.js
+++ script.js
@@ -73,13 +73,12 @@ function cellColor(td, line, column, snake, len, snakeLine, snakeCol, fruitLine,
 function Controller(snakeLine, snakeCol, fruitLine, fruitCol, snake, len) {
     let arrowRight = true, arrowUp = false, arrowLeft = false, arrowDown = false, interval; 
     const eatenFruit = {val: false}, isSnakeBody = {value: false};
-    interval = setInterval(function output() { if (arrowRight == true) {
-        let direction = 'right';  
+    let direction = 'right'; 
+    interval = setInterval(function output() { if (direction != 'left') {
         moveSnake(interval, snake, len, snakeLine, snakeCol, fruitLine, fruitCol,
             eatenFruit, isSnakeBody, direction);
     } }, 300);
     console.log(interval);
-    let direction = 'right';
     window.addEventListener("keydown", function move(event) {
         if (((event.key == 'ArrowRight') || (event.key == 'ArrowUp')
         || (event.key == 'ArrowLeft') || (event.key == 'ArrowDown')) &&
